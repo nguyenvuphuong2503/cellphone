bb2ec8f846d38beb49aa76909737d9c11169efb4
nguyenvuphuong2503 <124040508+nguyenvuphuong2503@users.noreply.github.com>
2023-02-17 20:30:51 +0700

    Add new project food

---
+++ assets/scrips.js
@@ -0,0 +1,8 @@
+const rightbtn =document.querySelector(".fa-chevron-right")
+console.log(rightbtn)
+rightbtn.addEventListener("click", function () {
+   document.querySelector(".slider-top-btn").syle.right ="100%"
+})
+
+
+          
